d189af959ef61dfa3900c574bfb9eeb8710d2f0e
Virtual <virtualll001@gmail.com>
2022-01-16 18:13:03 +0100

    Add project files.

---
+++ BlazorProj/Server/Models/IDepartmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using BlazorProj.Shared;
+
+namespace BlazorProj.Server.Models
+{
+    public interface IDepartmentRepository
+    {
+        Task<IEnumerable<Department>> GetDepartments();
+        Task<Department> GetDepartment(int departmentId);
+
+    }
+}
